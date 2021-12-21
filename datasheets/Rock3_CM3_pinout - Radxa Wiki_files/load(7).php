040e842b6b357f6a29f157bc557fb687f00b2602
Evan Krall <evan@evankrall.com>
2021-12-21 00:02:05 -0800

    Start refactoring board to use Radxa Rock 3 Compute Module instead of Raspberry Pi CM4.

---
+++ datasheets/Rock3_CM3_pinout - Radxa Wiki_files/load(7).php
@@ -0,0 +1,3 @@
+$('.lazyYT').lazyYT();$('#tweecool').tweecool({username:'radxa_oshw',limit:2,profile_image:true,show_time:false});$('.bs-example-tabs a').click(function(e){e.preventDefault()
+$(this).tab('show')});mw.loader.state({"site":"ready"});
+/* cache key: radxa_wiki-radxa_wiki:resourceloader:filter:minify-js:7:3a9d402434fc2c340c89498c7f015865 */
